de2fe53c98e5e9e619807779ed9c4e9904a0ffb9
Maximilian Schönenberg <schoenenberg@users.noreply.github.com>
2023-08-16 11:39:24 +0200

    refactor(ch-app): Moved rocket fairings in separate module in logging service, restructured keystore fairing

---
+++ clearing-house-app/logging-service/src/db/config/keyring_store.rs
@@ -0,0 +1,113 @@
+use anyhow::anyhow;
+use rocket::fairing::Kind;
+use rocket::{Build, fairing, Rocket};
+use core_lib::constants::{CLEAR_DB, DATABASE_URL, FILE_DEFAULT_DOC_TYPE, KEYRING_DB, KEYRING_DB_CLIENT};
+use core_lib::db::init_database_client;
+use core_lib::util::read_file;
+use crate::db::key_store::KeyStore;
+use crate::model::crypto::MasterKey;
+use crate::model::doc_type::DocumentType;
+
+#[derive(Clone, Debug)]
+pub struct KeyringDbConfigurator;
+
+#[rocket::async_trait]
+impl fairing::Fairing for KeyringDbConfigurator {
+    fn info(&self) -> fairing::Info {
+        fairing::Info {
+            name: "Configuring Keyring Database",
+            kind: Kind::Ignite
+        }
+    }
+    async fn on_ignite(&self, rocket: Rocket<Build>) -> fairing::Result {
+        let db_url: String = rocket.figment().extract_inner(DATABASE_URL).clone().unwrap();
+        let clear_db = match rocket.figment().extract_inner(CLEAR_DB) {
+            Ok(value) => {
+                debug!("clear_db: '{}' found.", &value);
+                value
+            },
+            Err(_) => {
+                false
+            }
+        };
+        debug!("Using database url: '{:#?}'", &db_url);
+
+        match Self::init_keystore(db_url, clear_db).await {
+            Ok(keystore) => {
+                Ok(rocket.manage(keystore))
+            },
+            Err(_) => {
+                Err(rocket)
+            }
+        }
+    }
+}
+
+impl KeyringDbConfigurator {
+    pub async fn init_keystore(db_url: String, clear_db: bool) -> anyhow::Result<KeyStore> {
+        debug!("Using database url: '{:#?}'", &db_url);
+
+        match init_database_client::<KeyStore>(&db_url.as_str(), Some(KEYRING_DB_CLIENT.to_string())).await {
+            Ok(keystore) => {
+                debug!("Check if database is empty...");
+                match keystore.client.database(KEYRING_DB)
+                    .list_collection_names(None)
+                    .await {
+                    Ok(colls) => {
+                        debug!("... found collections: {:#?}", &colls);
+                        if colls.len() > 0 && clear_db {
+                            debug!("Database not empty and clear_db == true. Dropping database...");
+                            match keystore.client.database(KEYRING_DB).drop(None).await {
+                                Ok(_) => {
+                                    debug!("... done.");
+                                }
+                                Err(_) => {
+                                    debug!("... failed.");
+                                    return Err(anyhow!("Failed to drop database"));
+                                }
+                            };
+                        }
+                        if colls.len() == 0 || clear_db {
+                            debug!("Database empty. Need to initialize...");
+                            debug!("Adding initial document type...");
+                            match serde_json::from_str::<DocumentType>(&read_file(FILE_DEFAULT_DOC_TYPE).unwrap_or(String::new())) {
+                                Ok(dt) => {
+                                    match keystore.add_document_type(dt).await {
+                                        Ok(_) => {
+                                            debug!("... done.");
+                                        },
+                                        Err(e) => {
+                                            error!("Error while adding initial document type: {:#?}", e);
+                                            return Err(anyhow!("Error while adding initial document type"));
+                                        }
+                                    }
+                                }
+                                _ => {
+                                    error!("Error while loading initial document type");
+                                    return Err(anyhow!("Error while loading initial document type"));
+                                }
+                            };
+                            debug!("Creating master key...");
+                            // create master key
+                            match keystore.store_master_key(MasterKey::new_random()).await {
+                                Ok(true) => {
+                                    debug!("... done.");
+                                },
+                                _ => {
+                                    error!("... failed to create master key");
+                                    return Err(anyhow!("Failed to create master key"));
+                                }
+                            };
+                        }
+                        debug!("... database initialized.");
+                        Ok(keystore)
+                    }
+                    Err(_) => {
+                        Err(anyhow!("Failed to list collections"))
+                    }
+                }
+            },
+            Err(_) => Err(anyhow!("Failed to initialize database client"))
+        }
+    }
+}
