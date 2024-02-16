aac60adf4bfbcbbd323163f2b673f6d7be47cdd3
dhommen <dh@orbiter.de>
2024-02-16 07:56:22 +0100

    chore(tests): extend ch-app smoke tests with create process

--- tests/smoke.js
+++ tests/smoke.js
@@ -1,6 +1,7 @@
 import http from 'k6/http';
 import { check } from 'k6';
 import logMessage from './util/logMessage.js';
+import createProcess from './util/createProcess.js';
 import header from './util/header.js'
 
 export const options = {
@@ -15,9 +16,19 @@ export default () => {
     'ch-app GET jwks is status 200': (r) => r.status === 200,
   });
 
+  const createProcessRes = http.post(`${url}/process/1`, JSON.stringify(createProcess(), null, 2), { headers: header() });
+  check(createProcessRes, {
+    'ch-app POST /process is status 201': (r) => r.status === 201,
+  });
 
-  const logMessageRes = http.post(`${url}/messages/log/6`, JSON.stringify(logMessage(), null, 2), { headers: header() });
+
+  const logMessageRes = http.post(`${url}/messages/log/1`, JSON.stringify(logMessage(), null, 2), { headers: header() });
   check(logMessageRes, {
-    'ch-app POST logmessage is status 201': (r) => r.status === 201,
+    'ch-app POST /messages/log is status 201': (r) => r.status === 201,
+  });
+
+  const logMessageResNew = http.post(`${url}/messages/log/2`, JSON.stringify(logMessage(), null, 2), { headers: header() });
+  check(logMessageResNew, {
+    'ch-app POST /messages/log is status 201': (r) => r.status === 201,
   });
 };
