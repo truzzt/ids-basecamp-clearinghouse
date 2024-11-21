019b5dde300919dde775d5614a72e32bb9c65601
Maximilian Schönenberg <schoenenberg@users.noreply.github.com>
2024-11-21 14:45:45 +0100

    Squashed all changes of Logginghouse 2.0

--- clearing-house-app/src/model/ids/request.rs
+++ clearing-house-app/src/model/ids/request.rs
@@ -1,10 +1,11 @@
 use crate::model::ids::message::IdsMessage;
 
-/// IDS Multipart message represented as a JSON struct
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+// IDS Multipart message represented as a JSON struct
+/*#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct ClearingHouseMessage {
     pub header: IdsMessage,
     pub payload: Option<String>,
     #[serde(rename = "payloadType")]
     pub payload_type: Option<String>,
 }
+*/
